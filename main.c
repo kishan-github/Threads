b5aaba8dbd1975f0feee279a7754448b9aa0e042
Kishan <kishankumar29@yahoo.com>
2019-08-24 22:47:23 +0530

    Basic code changes for condition variable. Need to fix the issues.

--- main.c
+++ main.c
@@ -1,41 +1,64 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
-#define MAX_THREAD 10
+#define MAX_THREAD 3
+#define MAX_VALUE 30
 
 int var = 10;
 pthread_mutex_t update_mutex;
+pthread_cond_t value_cond;
+
+// Routine which will wait for particular condition.
+void *wait_cond(void *arg)
+{
+	while(var < MAX_VALUE)
+	{
+		pthread_mutex_lock(&update_mutex);
+		pthread_cond_wait(&value_cond, &update_mutex);
+		printf("\nSignal received with value = %d\n", var);
+		pthread_mutex_unlock(&update_mutex);
+	}
+	pthread_exit(NULL);
+}
 
 // Routine to be executed when a thread is created.
 void *routine(void *arg)
 {
-	char *value = NULL;
-	static int check = 10;
+	while(var < MAX_VALUE)
+	{
+		pthread_mutex_lock(&update_mutex);
+		var++;
+		if((var%5) == 0)
+		{
+			pthread_cond_signal(&value_cond);
+			printf("\nSignal sent with value = %d, thread id = %lu\n", var, pthread_self());
+		}
 
-	value = (char *)arg;
+		printf("\nIn routine, Thread id = %lu, Global = %d\n", pthread_self(), var);
+		pthread_mutex_unlock(&update_mutex);
+		sleep(1);
+	}
 
-	pthread_mutex_lock(&update_mutex);
-	var++;
-	check = var;
-	pthread_mutex_unlock(&update_mutex);
-
-	printf("\nIn routine, Thread id = %lu, Global = %d, Static = %d\n", pthread_self(), var, check);
-	printf("Argument received = %s", value);
-	pthread_exit(&check);
+	pthread_exit(NULL);
 }
 
 // Main function.
 int main()
 {
-	pthread_t tid[MAX_THREAD];
+	pthread_t tid[MAX_THREAD], wait_tid;
 	int rt = -1;
 	char str[10] = "abcd";
 	int idx = 0;
-	int *thread_ret = NULL;
 
 	printf("\nIn main, Thread id = %lu\n", pthread_self());
 
 	pthread_mutex_init(&update_mutex, NULL);
+	pthread_cond_init(&value_cond, NULL);
+
+	rt = pthread_create(&wait_tid, NULL, wait_cond, NULL);
+	if(rt)
+		printf("\n%s : %d - Thread creation failed\n", __func__, __LINE__);
 
 	for(;idx < MAX_THREAD; idx++)
 	{
@@ -47,13 +70,14 @@ int main()
 		}
 	}
 
+	pthread_join(wait_tid, NULL);
 	for(idx = 0; idx < MAX_THREAD; idx++)
 	{
-		pthread_join(tid[idx], (void *)&thread_ret);
-		printf("\nValue returned = %d by thread = %lu\n", *thread_ret, tid[idx]);
+		pthread_join(tid[idx], NULL);
 	}
-	printf("\nThread finished = %s\n", str);
+
 	pthread_mutex_destroy(&update_mutex);
+	pthread_cond_destroy(&value_cond);
 	pthread_exit(NULL);
 	return 0;
 }
