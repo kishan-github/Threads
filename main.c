9c67f38a626c52706c4fefdb5c80a1c6b1255cbc
Kishan <kishankumar29@yahoo.com>
2019-08-24 20:45:19 +0530

    Accessing shared data between multiple threads.

--- main.c
+++ main.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define MAX_THREAD 10
+
+int var = 10;
+
 // Routine to be executed when a thread is created.
 void *routine(void *arg)
 {
 	char *value = NULL;
+	static int check = 10;
 
 	value = (char *)arg;
-	printf("\nIn routine, Thread id = %lu\n", pthread_self());
+	var++;
+	check++;
+
+	printf("\nIn routine, Thread id = %lu, Global = %d, Static = %d\n", pthread_self(), var, check);
 	printf("\nArgument received = %s\n", value);
-	pthread_exit(NULL);
+	pthread_exit(&check);
 }
 
 // Main function.
 int main()
 {
-	pthread_t tid;
+	pthread_t tid[MAX_THREAD];
 	int rt = -1;
 	char str[10] = "abcd";
+	int idx = 0;
+	int *thread_ret = NULL;
 
 	printf("\nIn main, Thread id = %lu\n", pthread_self());
-	rt = pthread_create(&tid, NULL, routine, (void *)str);
-	if(rt)
+
+	for(;idx < MAX_THREAD; idx++)
 	{
-		printf("\nThread creation failed\n");
-		return 0;
+		rt = pthread_create(&tid[idx], NULL, routine, (void *)str);
+		if(rt)
+		{
+			printf("\nThread creation failed\n");
+			return 0;
+		}
 	}
-	pthread_join(tid, NULL);
+
+	for(idx = 0; idx < MAX_THREAD; idx++)
+	{
+		pthread_join(tid[idx], (void *)&thread_ret);
+		printf("\nValue returned = %d by thread = %lu\n", *thread_ret, tid[idx]);
+	}
 	printf("\nThread finished = %s\n", str);
 	pthread_exit(NULL);
 	return 0;
